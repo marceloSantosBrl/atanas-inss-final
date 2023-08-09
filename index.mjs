ab4c49c5bd86ddf364f7bed22d0b932bc0b560a0
marcelo <marcelegald2@outlook.com>
2023-08-08 21:11:10 -0300

    fix pedidos

--- index.mjs
+++ index.mjs
@@ -77,25 +77,78 @@ const ativarBloco = (value, estado) => {
         'grid' : 'hidden');
 }
 
-const obterPorcentagemClt = value => {
-    if (value <= 1_100) return 0.075;
-    if (value > 1_100 && value <= 2_203.48) return 0.09;
-    if (value > 2_203.48 && value <= 3_305.22) return 0.12;
-    if (value > 3_305.22 && value <= 6_433.57) return 0.14;
-    throw new Error('Valor Fora de Escopo');
+// const obterPorcentagemClt = value => {
+//     if (value <= 1_100) return 0.075;
+//     if (value > 1_100 && value <= 2_203.48) return 0.09;
+//     if (value > 2_203.48 && value <= 3_305.22) return 0.12;
+//     if (value > 3_305.22 && value <= 6_433.57) return 0.14;
+//     throw new Error('Valor Fora de Escopo');
+// }
+
+const obterTotalClt = valor => {
+    if (valor < 0) throw new Error('Salário Inválido');
+    const arrayPisos = [1_320, 2_571.29, 3_856.94, 7_507.49]
+    const aliquotas = [0.075, 0.09, 0.12, 0.14]
+    let arrayValores = [0, 0, 0, 0]
+    if (valor <= arrayPisos[0]) {
+        arrayValores[0] = valor;
+    }
+    if (valor > arrayPisos[0] && valor <= arrayPisos[1]) {
+        arrayValores[0] = arrayPisos[0]
+        arrayValores[1] = valor - arrayValores[0]
+    }
+    if (valor > arrayPisos[1] && valor <= arrayPisos[2]) {
+        arrayValores[0] = arrayPisos[0];
+        arrayValores[1] = arrayPisos[1] - arrayPisos[0]
+        arrayValores[2] = valor - arrayPisos[1]
+    }
+    if (valor > 3_305.22 && valor <= 6_433.57) {
+        arrayValores[0] = arrayPisos[0];
+        arrayValores[1] = arrayPisos[1] - arrayPisos[0]
+        arrayValores[2] = arrayPisos[2] - arrayPisos[1]
+        arrayValores[3] = valor - arrayPisos[2]
+    }
+    if (valor >= 6_433.57) {
+        arrayValores[0] = arrayPisos[0];
+        arrayValores[1] = arrayPisos[1] - arrayPisos[0]
+        arrayValores[2] = arrayPisos[2] - arrayPisos[1]
+        arrayValores[3] = arrayPisos[3] - arrayPisos[2]
+    }
+    let total = 0;
+    for (let i = 0; i < 4; ++i) {
+        total += aliquotas[i] * arrayValores[i];
+    }
+    return total;
 }
 
-const obterPorcentagemAutonomo = value => {
-    if (value >= 0 && value <= 6_433.57) return 0.2;
-    throw new Error('Valor Fora de Escopo');
+
+// const obterPorcentagemAutonomo = value => {
+//     if (value >= 0 && value <= 6_433.57) return 0.2;
+//     throw new Error('Valor Fora de Escopo');
+// }
+//
+// const obterProLabore = value => {
+//     if (value >= 0 && value <= 6_433.57) return 0.11;
+//     throw new Error('Valor Fora de Escopo');
+// }
+//
+// const obterTotal = (valor, funcao) => valor * funcao(valor);
+
+const ObterTotalAutonomo = valor => {
+    if (valor < 0) throw new Error('Salário Inválido');
+    if (valor > 7_507.49) {
+        return 7_507.49 * 0.20;
+    }
+    return valor * 0.20;
 }
 
-const obterProLabore = value => {
-    if (value >= 0 && value <= 6_433.57) return 0.11;
-    throw new Error('Valor Fora de Escopo');
+const ObterTotalProLabore = valor => {
+    if (valor < 0) throw new Error('Salário Inválido');
+    if (valor > 7_507.49) {
+        return 7_507.49 * 0.11;
+    }
+    return valor * 0.11;
 }
-
-const obterTotal = (valor, funcao) => valor * funcao(valor);
 
 const obterTexto = valor => `Quanto você deve pagar de Inss por mês:\n R$ ${valor.toLocaleString("pt-BR", {maximumFractionDigits: 2, minimumFractionDigits: 2})}`
 
@@ -106,14 +159,14 @@ formulario.addEventListener('submit', (event) => {
     const data = new FormData(formulario);
     const valorSalario = data.get("salario");
     try {
-        const valorClt = obterTotal(valorSalario, obterPorcentagemClt)
-        const valorAutonomo = obterTotal(valorSalario, obterPorcentagemAutonomo)
-        const valorProLabore = obterTotal(valorSalario, obterProLabore)
+        const valorClt = obterTotalClt(valorSalario)
+        const valorAutonomo = ObterTotalAutonomo(valorSalario);
+        const valorProLabore = ObterTotalProLabore(valorSalario);
         document.getElementById("cltSaida").innerText = obterTexto(valorClt);
         document.getElementById("autonomoSaida").innerText = obterTexto(valorAutonomo);
         document.getElementById("proLaboreSaida").innerText = obterTexto(valorProLabore);
     } catch (e) {
-        const mensagem = "Salário fora dos limites de cálculo"
+        const mensagem = "Salário Inválido"
         document.getElementById("cltSaida").innerText = mensagem;
         document.getElementById("autonomoSaida").innerText = mensagem;
         document.getElementById("proLaboreSaida").innerText = mensagem;
